750882a36d0f2662a13baea311ea7ca4063f5c51
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-04-10 19:17:49 +0600

    Price

---
+++ resources/views/backend/website/price/index.blade.php
@@ -0,0 +1,107 @@
+@push('title')
+    Price
+@endpush
+@extends('layouts.backend.app')
+@push('style')
+
+@endpush
+@section('breadcrumb')
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h4 class="text-themecolor">Price</h4>
+        </div>
+        <div class="col-md-7 align-self-center text-right">
+            <div class="d-flex justify-content-end align-items-center">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('backend.dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Price</li>
+                </ol>
+                <a href="{{ route('backend.price.create') }}" class="btn btn-info d-none d-lg-block m-l-15"><i class="fa fa-plus-circle"></i> Create Price</a>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('content')
+    <!-- Start Contentbar -->
+    <div class="contentbar">
+        <!-- Start row -->
+        <div class="row">
+            <!-- Start col -->
+            <div class="col-lg-12">
+                <div class="card m-b-30">
+                    <div class="card-header">
+                        <h5 class="card-title">Price </h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="datatable" class="display table table-striped table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Duration</th>
+                                    <th>Price</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Duration</th>
+                                    <th>Price</th>
+                                    <th>Name</th>
+                                </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End col -->
+        </div>
+        <!-- End row -->
+    </div>
+    <!-- End Contentbar -->
+@endsection
+@push('script')
+
+    <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(function() {
+            $('#datatable').DataTable({
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                ajax: '{!! route('backend.price.index') !!}',
+                columns: [{
+                    data: 'name',
+                    name: 'name'
+                },{
+                    data: 'duration',
+                    name: 'duration'
+                },{
+                    data: 'price',
+                    name: 'price'
+                },
+                {
+                    data: 'action',
+                    name: 'action'
+                },
+                ],
+                initComplete: function() {
+                    this.api().columns().every(function() {
+                        var column = this;
+                        var input = document.createElement("input");
+                        $(input).appendTo($(column.footer()).empty())
+                            .on('change', function() {
+                                column.search($(this).val(), false, false, true).draw();
+                            });
+                    });
+                }
+            });
+        });
+
+    </script>
+@endpush
